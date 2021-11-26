1488242f643f5b87a87b80cd5b594b54e53075aa
Dmitrii Kazaiev <dmytrokazaev@gmail.com>
2021-11-26 21:57:03 +0200

    [03-01] Implement LimitSizeStream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.passedBytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.passedBytes += chunk.length;
 
+    if (this.passedBytes > this.limit) {
+      return callback(new LimitExceededError());
+    }
+
+    return callback(null, chunk);
   }
 }
 
