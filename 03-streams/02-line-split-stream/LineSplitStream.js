541d6fa3a27ac2fa487547d9c8375c16a3499a11
Dmitrii Kazaiev <dmytrokazaev@gmail.com>
2021-11-27 02:22:32 +0200

    [03-02] Implement LineSplitStream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,13 +4,26 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.stringTail = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.stringTail += chunk;
+    const strings = this.stringTail.toString().split(os.EOL);
+
+    this.stringTail = strings.pop();
+    if (strings.length > 0) {
+      callback(null, strings.shift());
+      strings.forEach((str) => this.push(str));
+    } else {
+      callback(null, null);
+    }
   }
 
   _flush(callback) {
+    callback(null, this.stringTail);
   }
 }
 
 module.exports = LineSplitStream;
+
