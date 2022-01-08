b13ee471c591c6dd8185c2283f53bbdc73ddf54f
Dmitrii Kazaiev <dmytrokazaev@gmail.com>
2022-01-08 22:51:02 +0200

    [06-02] Add Category and Product endpoints

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,39 @@
+const mapProduct = require('../mappers/product');
+const Product = require('../models/Product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const products = (await Product.find({
+    subcategory,
+  })).map(mapProduct);
+
+  ctx.body = {products};
 };
 
-module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+module.exports.productList = async function productList(ctx) {
+  const products = (await Product.find()).map(mapProduct);
+
+  ctx.body = {products};
 };
 
-module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+const ObjectId = require('mongoose').Types.ObjectId;
+
+module.exports.productById = async function productById(ctx) {
+  const {id} = ctx.params;
+  try {
+    const product = await Product.findOne({_id: new ObjectId(id)});
+
+    if (!product) {
+      ctx.status = 404;
+      return;
+    }
+
+    ctx.body = {product: mapProduct(product)};
+  } catch (e) {
+    ctx.status = 400;
+  }
 };
 
